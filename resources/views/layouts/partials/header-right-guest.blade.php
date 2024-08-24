e80cf5fc7d5b4dae608a99681d2312eb616a4155
EslamKamel89 <eslam_kamel_89@hotmail.com>
2024-08-24 12:49:45 +0300

    creating project main app and guest layout

---
+++ resources/views/layouts/partials/header-right-guest.blade.php
@@ -0,0 +1,6 @@
+<a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500" href="{{route( 'login' )}}">
+    Login
+</a>
+<a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500" href="{{route( 'logout' )}}">
+    Register
+</a>
